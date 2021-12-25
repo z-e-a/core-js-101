9940b08b645bcb82ff904325f27b92f99cee9c6e
z-e-a <e.a.zubkov@yandex.ru>
2021-12-26 01:58:33 +0300

    feat: solve task 'extractNameFromTemplate should parse the name from given string'

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -65,8 +65,8 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  return value.replace(/Hello, /, '').replace(/!/, '');
 }
 
 
