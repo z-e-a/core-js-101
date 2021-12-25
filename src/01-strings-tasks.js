dd50789794e62ea7307b87bd9c1c611cea890718
z-e-a <e.a.zubkov@yandex.ru>
2021-12-26 02:01:58 +0300

    feat: solve task 'removeLeadingAndTrailingWhitespaces should remove leading and trailing whitespaces from the string'

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -95,8 +95,8 @@ function getFirstChar(value) {
  *   'cat'              => 'cat'
  *   '\tHello, World! ' => 'Hello, World!'
  */
-function removeLeadingAndTrailingWhitespaces(/* value */) {
-  throw new Error('Not implemented');
+function removeLeadingAndTrailingWhitespaces(value) {
+  return value.trim();
 }
 
 /**
