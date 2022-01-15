066c5b5cf279fe5dc6d212112181d8827c11199f
z-e-a <e.a.zubkov@yandex.ru>
2022-01-16 02:58:55 +0300

    feat: solve task 'getMovingSum'

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -234,8 +234,9 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  let accum = 0;
+  return arr.map((el) => { accum += el; return accum; });
 }
 
 /**
