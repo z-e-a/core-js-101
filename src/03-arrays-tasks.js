83096e0f01e22f57ae4954a5d3b53a9fd908bd4f
z-e-a <e.a.zubkov@yandex.ru>
2022-01-15 17:47:46 +0300

    feat: solve task 'getArrayOfPositives'

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -68,8 +68,8 @@ function doubleArray(arr) {
  *    [-1, 2, -5, -4, 0] => [ 2 ]
  *    [] => []
  */
-function getArrayOfPositives(/* arr */) {
-  throw new Error('Not implemented');
+function getArrayOfPositives(arr) {
+  return arr.filter((el) => el > 0);
 }
 
 /**
