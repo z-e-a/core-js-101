5bb5cbe0b378ef3e69aa59fc7761f44e634f5b0d
z-e-a <e.a.zubkov@yandex.ru>
2022-01-16 14:04:14 +0300

    feat: solve task 'getFalsyValuesCount'

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -355,8 +355,8 @@ function getItemsSum(arr) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.reduce((prev, curr) => prev + (!curr ? 1 : 0), 0);
 }
 
 /**
