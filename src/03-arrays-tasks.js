16adf8ac04543a449e4b36c560df02f14616b97d
z-e-a <e.a.zubkov@yandex.ru>
2022-01-16 03:15:20 +0300

    feat: solve task 'getSecondItems'

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -250,8 +250,8 @@ function getMovingSum(arr) {
  * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
  * [ "a" ] => []
  */
-function getSecondItems(/* arr */) {
-  throw new Error('Not implemented');
+function getSecondItems(arr) {
+  return arr.filter((el, ind) => ind % 2 !== 0);
 }
 
 
