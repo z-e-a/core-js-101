870dac1e1ee50a0f30ed187dac4e160ec05f60da
z-e-a <e.a.zubkov@yandex.ru>
2022-01-16 14:10:54 +0300

    feat: solve task 'findAllOccurrences'

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -373,8 +373,8 @@ function getFalsyValuesCount(arr) {
  *    [ null, undefined, null ], null => 2
  *    [ true, 0, 1, 'true' ], true => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.reduce((prev, curr) => prev + (curr === item ? 1 : 0), 0);
 }
 
 /**
