23748cca8d36d6f651d9b15c445507cdcbb32fb3
z-e-a <e.a.zubkov@yandex.ru>
2022-01-16 03:37:14 +0300

    feat: solve task 'propagateItemsByPositionIndex'

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -269,8 +269,8 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr.flatMap((el, ind) => Array(ind + 1).fill(el));
 }
 
 
