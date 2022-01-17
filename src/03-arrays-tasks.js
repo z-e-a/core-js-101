98ccacf8cbd3a359f9716641d46f37c4015552e5
z-e-a <e.a.zubkov@yandex.ru>
2022-01-17 14:54:42 +0300

    feat: solve task 'swapHeadAndTail'

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -600,8 +600,9 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const middle = arr.length / 2;
+  return [].concat(arr.slice(-middle), arr.slice(middle, -middle), arr.slice(0, middle));
 }
 
 
