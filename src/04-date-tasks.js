af9ea8fb6e8d021ba72310afe57a9fd81a20906f
z-e-a <e.a.zubkov@yandex.ru>
2022-01-17 17:18:27 +0300

    feat: solve task 'timeSpanToString'

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -74,8 +74,12 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const span = new Date(endDate - startDate);
+  return `${span.getUTCHours().toString().padStart(2, '0')}:${
+    span.getUTCMinutes().toString().padStart(2, '0')}:${
+    span.getUTCSeconds().toString().padStart(2, '0')}.${
+    span.getUTCMilliseconds().toString().padStart(3, '0')}`;
 }
 
 
