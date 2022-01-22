87858fc5feb0146ff6f141725ffaf311027afed5
z-e-a <e.a.zubkov@yandex.ru>
2022-01-22 17:58:15 +0300

    feat: solve task 'getDigitalRoot'

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -297,8 +297,14 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  let result;
+  if (String(num).length < 2) {
+    result = num;
+  } else {
+    result = getDigitalRoot(String(num).split('').reduce((sum, el) => sum + +el, 0));
+  }
+  return result;
 }
 
 
